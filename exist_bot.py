fb9f606ffaa22767ab5611bce9cfb2a516b0a464
smallwest888 <63802737+smallwest888@users.noreply.github.com>
2023-11-22 17:11:34 +0100

    File Reader_GPT 3.5 with Easier way to Update Docs

--- exist_bot.py
+++ exist_bot.py
@@ -2,6 +2,7 @@ import os
 import PyPDF2
 import openai
 import streamlit as st
+import re
 
 st.set_page_config(page_title="IPRO-Chatbot", page_icon="IPRO-CHAT.png", layout="wide")
 example_questions = ["Wie soll ich den Standort des Zimmers finden?", "Wie soll ich den Professor kontaktieren?",
@@ -27,7 +28,7 @@ client = openai.Client()
 # set_background_color("#9CC4CC")
 
 
-BASE_DIR = "Files"  # Set the base directory to "Files"
+BASE_DIR = os.getcwd()  # Set the base directory to "Files"
 
 
 def generate_response(user_input):
@@ -96,8 +97,7 @@ def get_pdf_content(file_path):
 
 
 def predict_intent_with_gpt(question):
-    valid_intents = ["Contact", "Transport", "Main", "Stipendium", "Studiengänge", "Hochschule-Grunddaten",
-                     "Promovieren", "Person"]
+    # Use the updated valid_intents list
     max_attempts = 1
     attempts = 0
     while attempts < max_attempts:
@@ -105,9 +105,8 @@ def predict_intent_with_gpt(question):
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system",
-                 "content": "Predict the intent of the question. The answer must be one of the following: 'Contact', "
-                            "'Stipendium', 'Studiengänge', 'Hochschule-Grunddaten', 'Promovieren' , 'Person' or "
-                            "'Main'."},
+                 "content": "Predict the intent of the question. The answer must be one of the following: " +
+                            ", ".join(valid_intents) + "."},
                 {"role": "user", "content": question},
             ]
         )
@@ -115,15 +114,30 @@ def predict_intent_with_gpt(question):
         predicted_intent = response.choices[0].message.content.strip()
 
         if predicted_intent in valid_intents:
-            pdf_file_path = os.path.join(BASE_DIR, predicted_intent, predicted_intent + ".pdf")
-            return pdf_file_path
+            # Adjust the path to match the required format
+            pdf_file_path = os.path.join("Files", predicted_intent, predicted_intent + ".pdf")
+            if os.path.exists(pdf_file_path):
+                return pdf_file_path
+            else:
+                print(f"File not found: {pdf_file_path}")
+                break  # Exit the loop if file is not found
         attempts += 1
-    return os.path.join(BASE_DIR, "Main", "Main.pdf")
+
+    # Default to a general PDF if the specific one is not found
+    return os.path.join("Files", "Main", "Main.pdf")
 
 
 # Initialize chat history in session state if it doesn't exist
 if 'messages' not in st.session_state:
     st.session_state.messages = []
+
+def read_valid_intents(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.read().splitlines()
+    return [line.strip() for line in lines if line.strip()]
+
+valid_intents_file = "valid_intents.txt"  # Assuming this is the name of your text file
+valid_intents = read_valid_intents(valid_intents_file)
 
 
 def handle_example_question(question):
